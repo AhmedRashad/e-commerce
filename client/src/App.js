98ff12580c1f3cd799e3e7eeeb1fa55233ce442c
AhmedRashad <ahmedrabie441@gmail.com>
2022-03-28 16:23:57 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Register/Register.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Register.css";
+
+function Register() {
+  return <div>Register</div>;
+}
+
+export default Register;
