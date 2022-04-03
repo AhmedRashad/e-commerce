38b6b97bf5f4ecf009c17b15eb27fcbb525ecc64
Mostafa Elsaadawy <82791801+Mostafa-Alsadawy@users.noreply.github.com>
2022-04-03 03:23:31 +0200

    Update Button.jsx

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = (props)=>{
     return (
         <div class="grid place-content-center">
         <button
-          class="rounded border-2 border text-white bg-purple-700 hover:bg-transparent hover:text-purple-700 hover:border-purple-700 hover:border-2 mx-auto my-2 p-2"
+          class="rounded border-2 border-transparent border-solid text-white bg-purple-700 hover:bg-transparent hover:text-purple-700 hover:border-purple-700 hover:border-2 mx-auto my-2 p-2"
         >
           {props.children}
         </button>
