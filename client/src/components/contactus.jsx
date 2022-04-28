a15d0e383db752080338dbfe8fe7d2fd14f697e7
Mostafa-Alsadawy <mostafa.alsadawy2021@gmail.com>
2022-04-28 11:40:50 +0200

    end of my work today

--- client/src/components/contactus.jsx
+++ client/src/components/contactus.jsx
@@ -28,7 +28,7 @@ const ConstactUs = () => {
               </div>
             </div>
             <div className="col-span-2">
-              <label className="text-gray-700" for="name">
+              <label className="text-gray-700" htmlFor="name">
                 <textarea
                   className="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
                   id="comment"
