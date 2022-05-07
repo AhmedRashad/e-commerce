4ad9a1646c5259b0fb185cd47e69730ab2f36f78
Mostafa-Alsadawy <mostafa.alsadawy2021@gmail.com>
2022-05-07 10:31:22 +0200

    fix logout on dashboard

--- client/src/components/DashBoard/components/topbar.jsx
+++ client/src/components/DashBoard/components/topbar.jsx
@@ -1,15 +1,28 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, getUser } from "../../../features/auth/authSlice";
+
 import {
   HiOutlineLogout,
   HiOutlineBell,
   HiOutlineCog,
 } from "react-icons/hi";
-import { Link } from "react-router-dom";
+import { Link ,useNavigate} from "react-router-dom";
 
 const TopBar = (props) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+ 
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+
+
   return (
     <header className=" w-full py-2 px-4 bg-gray-200 opacity-75 sticky top-0 right-0">
       <div className="flex flex-row-reverse gap-2 sm:gap-4 items-center">
-        <Link
+        <button  onClick={handleLogout}
           className="group flex justify-center items-center gap-2 p-2 bg-transparent border-2 border-transparent md:hover:border-accent border-solid rounded-lg duration-100"
           to="/"
         >
@@ -17,7 +30,7 @@ const TopBar = (props) => {
             Log out
           </span>
           <HiOutlineLogout className="w-6 h-6 group-hover:text-accent duration-100" />
-        </Link>
+        </button>
 
         <Link to="settings"><HiOutlineCog  className="w-6 h-6 hover:text-accent duration-100" /></Link>
 
