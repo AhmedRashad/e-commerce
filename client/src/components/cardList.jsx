5825f7253321e4fba45b968ac3e7228a2867fd3e
Mostafa-Alsadawy <mostafa.alsadawy2021@gmail.com>
2022-05-07 11:39:05 +0200

    add to cart in home page

--- client/src/components/cardList.jsx
+++ client/src/components/cardList.jsx
@@ -1,4 +1,4 @@
-import { Link  } from "react-router-dom";
+import { Link } from "react-router-dom";
 const CardList = (props) => {
   return (
     <div className="container px-4">
@@ -14,7 +14,9 @@ const CardList = (props) => {
       </div>
 
       <div className="w-full flex flex-row-reverse pl-8">
-        <Link className="hover:text-indigo-500" to="/products">See all products</Link>
+        <Link className="hover:text-indigo-500" to="/products">
+          See all products
+        </Link>
       </div>
     </div>
   );
@@ -22,31 +24,31 @@ const CardList = (props) => {
 
 const InnerCard = (props) => {
   return (
-      <Link to={"/overview/"+props.product._id}>
-    <div class="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
-      <a href="#" className="w-full block h-full">
-        <img
-          className="max-h-40 w-full object-cover"
-          src={props.product.image[0]}
-          alt="product image"
-        />
-        <div className="bg-white dark:bg-gray-800 w-full p-4">
-          <p className="text-gray-800 dark:text-white text-xl font-medium mb-2">
-            {props.product.name}
-          </p>
-          <p className="text-gray-400 dark:text-gray-300 font-light text-md">
-            {props.product.description}
-          </p>
-        </div>
+    <>
+      <div class="overflow-hidden shadow-lg rounded-lg h-90 w-60 md:w-80 cursor-pointer m-auto">
+        <Link to={"/overview/" + props.product._id}>
+          <img
+            className="max-h-40 w-full object-cover"
+            src={props.product.image[0]}
+            alt="product image"
+          />
+          <div className="bg-white dark:bg-gray-800 w-full px-4">
+            <p className="text-gray-800 dark:text-white text-xl font-medium mb-2">
+              {props.product.name}
+            </p>
+            <p className="text-gray-400 dark:text-gray-300 font-light text-md">
+              {props.product.description}
+            </p>
+          </div>
+        </Link>
         <button
-                type="submit"
-                className="mt-10 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Add to cart
-              </button>
-      </a>
-    </div>
-    </Link>
+          type="submit"
+          className="my-4 mx-auto w-[80%] bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Add to cart
+        </button>
+      </div>
+    </>
   );
 };
 
